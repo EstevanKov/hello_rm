a327362a587ba3135bbdc42992cdfac65cf9b325
carlos-perez4 <135867428+carlos-perez4@users.noreply.github.com>
2024-09-06 19:01:38 -0600

    aa<

--- app/screens/home.tsx
+++ app/screens/home.tsx
@@ -1,21 +1,35 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
-export default function HomeScreen(){
-
-  
-
-     return (
-    <View>
-    <View
-      style={{
-        flexDirection: 'row',
-        height: 60,
-        width: 60,
-        backgroundColor: "green"
-      }}>
-      
-    </View>
+export default function HomeScreen() {
+  return (
+    <View style={estilos.container}>
+      <View style={estilos.azul}></View>
+      <View style={estilos.naranja}></View>
+      <View style={estilos.rojo}></View>
     </View>
   );
-};
+}
+
+const estilos= StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  azul: {
+    height: 60,
+    width: '50%',
+    backgroundColor: 'blue',
+  },
+  naranja: {
+    height: 60,
+    width: '75%',
+    backgroundColor: 'orange',
+  },
+  rojo: {
+    height: 60,
+    width: '100%',
+    backgroundColor: 'red',
+  },
+});
