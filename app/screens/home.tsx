ca100570ca21c2f0f1c2643e7aea0c3816c376d5
carlos-perez4 <135867428+carlos-perez4@users.noreply.github.com>
2024-09-06 18:45:41 -0600

    qq

---
+++ app/screens/home.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function HomeScreen(){
+
+  
+
+     return (
+    <View>
+    <View
+      style={{
+        flexDirection: 'row',
+        height: 60,
+        width: 60,
+        backgroundColor: "green"
+      }}>
+      
+    </View>
+    </View>
+  );
+};
