36d0e40884bb616820dc280c55e17864d50e5627
carlos-perez4 <135867428+carlos-perez4@users.noreply.github.com>
2024-09-06 19:15:40 -0600

    scroll

--- app/screens/home.tsx
+++ app/screens/home.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <View style={estilos.container}>
-      <View style={estilos.azul}></View>
-      <View style={estilos.naranja}></View>
-      <View style={estilos.rojo}></View>
-    </View>
+    <ScrollView>
+      <View style={estilos.container}>
+        <View style={estilos.azul}></View>
+        <View style={estilos.naranja}></View>
+        <View style={estilos.rojo}></View>
+        <View style={estilos.azul}></View>
+        <View style={estilos.naranja}></View>
+        <View style={estilos.rojo}></View>
+        <View style={estilos.azul}></View>
+        <View style={estilos.naranja}></View>
+        <View style={estilos.rojo}></View>
+        <View style={estilos.azul}></View>
+        <View style={estilos.naranja}></View>
+        <View style={estilos.rojo}></View>
+      </View>
+    </ScrollView>
   );
 }
 
-const estilos= StyleSheet.create({
+const estilos = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
